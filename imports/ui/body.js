7f8791621b81621da647ad1d6c36bb210a0fa729
samda <muy.samda13@gmail.com>
2016-12-20 08:24:11 +0700

    new app with meteor

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,11 +1,26 @@
 import { Template } from 'meteor/templating';
+import { Tasks } from '../api/tasks.js';
 
 import './body.html';
 
-Template.body.helper({
-  tasks: [
-    { text: 'This is task 1' },
-    { text: 'This is task 2' },
-    { text: 'This is task 3' },
-  ],
+Template.body.helpers({
+  tasks() {
+    return Tasks.find({});
+  },
+});
+
+Template.body.events({
+  'submit .new-task'(event) {
+    event.preventDefault();
+
+    const target = event.target;
+    const text = target.text.value;
+
+    Tasks.insert({
+      text,
+      createAt: new Date(),
+    });
+
+    target.text.value = '';
+  },
 });
